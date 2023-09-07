2554269ffd4909cc26858398381c0694d52f7d35
AllanMuhari <allanmuhari087@gmail.com>
2023-09-07 18:28:17 +0300

    functionalities

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-// Function to update the current day and time
-function updateDateTime() {
-    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const currentDate = new Date();
-    const currentDay = daysOfWeek[currentDate.getUTCDay()];
-    const currentTime = currentDate.getTime();
+/** @format */
 
-    document.getElementById("currentDay").textContent = currentDay;
-    document.getElementById("currentTime").textContent = currentTime;
+function updateDateTime() {
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const currentDate = new Date();
+  const currentDay = daysOfWeek[currentDate.getUTCDay()];
+  const currentTime = currentDate.getTime();
+
+  document.getElementById("currentDay").textContent = currentDay;
+  document.getElementById("currentTime").textContent = currentTime;
 }
 
-// Update date and time every second
 setInterval(updateDateTime, 1000);
 
-// Initial update
 updateDateTime();
