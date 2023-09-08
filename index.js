23dfc35c76e44592d860318f5c1b591f6dd4606f
AllanMuhari <allanmuhari087@gmail.com>
2023-09-08 09:41:42 +0300

    js file

--- index.js
+++ index.js
@@ -12,7 +12,14 @@ function updateDateTime() {
   ];
   const currentDate = new Date();
   const currentDay = daysOfWeek[currentDate.getUTCDay()];
-  const currentTime = currentDate.getTime();
+  const currentTime =
+    currentDate.getUTCHours() +
+    ":" +
+    currentDate.getUTCMinutes() +
+    ":" +
+    currentDate.getUTCSeconds() +
+    ":" +
+    currentDate.getUTCMilliseconds();
 
   document.getElementById("currentDay").textContent = currentDay;
   document.getElementById("currentTime").textContent = currentTime;
